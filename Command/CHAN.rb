9109a2ed85fcea31d84a8d141d69544fc68f62b1
Ghardo <jerturo@gmail.com>
2013-10-15 20:43:21 +0200

    Update CHAN.rb
    Fixing problems

--- Command/CHAN.rb
+++ Command/CHAN.rb
@@ -1,31 +1,31 @@
 #!/usr/bin/env ruby
 class SVDRP::Command::CHAN < SVDRP::BaseCommand
   def next
-    result self.send "CHAN +"
+    self.send "CHAN +"
   end
 
   def prev
-    result self.send "CHAN -"
+    self.send "CHAN -"
   end
 
   def setByNumber(number)
-    return self._setByArg number
+    self._setByArg number
   end
 
   def setById(id)
-     return self._setByArg id
+     self._setByArg id
   end
 
   def setByName(name)
-     return self._setByArg name
+     self._setByArg name
   end
 
 	def get
-		return self.send "CHAN"
+		return self.send("CHAN").last
 	end
 
 	protected
   def _setByArg(arg)
-     result self.send "CHAN #{arg}"
+     self.send "CHAN #{arg}"
   end
 end
